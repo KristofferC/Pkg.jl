816025e8bd24c22e6d32aaaa9f368fcae6eea1a9
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-11-24 08:00:20 -0800

    test Pkg.test (#2)
    * test Pkg.test
    
    * test Crayons instead
    
    * deprec fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,31 +1,32 @@
 using Pkg3
-using Base.Test
+if Base.isdeprecated(Base, :Test)
+    using Test
+else
+    using Base.Test
+end
 using Pkg3.Types
 
 function temp_pkg_dir(fn::Function)
-    local depot
     local project_path
     try
-        depot = joinpath(tempdir(), randstring())
-        empty!(Pkg3.DEPOTS)
-        push!(Pkg3.DEPOTS, depot)
         project_path = joinpath(tempdir(), randstring())
 
         withenv("JULIA_ENV" => project_path) do
             fn()
         end
     finally
-        rm(depot, recursive=true, force=true)
         rm(project_path, recursive=true, force=true)
     end
 end
 
+const TEST_PKG = "Crayons"
+
 temp_pkg_dir() do
-    Pkg3.add("Example")
-    @eval import Example
+    Pkg3.add(TEST_PKG)
+    @eval import $(Symbol(TEST_PKG))
     Pkg3.update()
-    Pkg3.test("Example")
-    Pkg3.rm("Example")
+    Pkg3.test(TEST_PKG)
+    Pkg3.rm(TEST_PKG)
 
     nonexisting_pkg = randstring(14)
     @test_throws CommandError Pkg3.API.add(nonexisting_pkg)
