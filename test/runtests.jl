f4eea392970f3d2d6ba43e640fd8f29409ddfbfe
Kristoffer Carlsson <kcarlsson89@gmail.com>
2017-11-24 14:11:30 +0100

    test Pkg.test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,28 +3,24 @@ using Base.Test
 using Pkg3.Types
 
 function temp_pkg_dir(fn::Function)
-    local depot
     local project_path
     try
-        depot = joinpath(tempdir(), randstring())
-        empty!(Pkg3.DEPOTS)
-        push!(Pkg3.DEPOTS, depot)
         project_path = joinpath(tempdir(), randstring())
 
         withenv("JULIA_ENV" => project_path) do
             fn()
         end
     finally
-        rm(depot, recursive=true, force=true)
         rm(project_path, recursive=true, force=true)
     end
 end
 
 temp_pkg_dir() do
-    Pkg3.API.add("Example")
+    Pkg3.add("Example")
     @eval import Example
-    Pkg3.API.up()
-    Pkg3.API.rm("Example")
+    Pkg3.update()
+    Pkg3.test("Example")
+    Pkg3.rm("Example")
 
     nonexisting_pkg = randstring(14)
     @test_throws CommandError Pkg3.API.add(nonexisting_pkg)
