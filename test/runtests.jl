be86075c969a01f21ed26a5f29111670039cd51d
Kristoffer Carlsson <kcarlsson89@gmail.com>
2017-11-24 17:26:39 +0100

    test that resolve failure reports the pkg that failed

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,6 +28,12 @@ temp_pkg_dir() do
     Pkg3.test(TEST_PKG)
     Pkg3.rm(TEST_PKG)
 
+    try
+        Pkg3.API.add([PackageSpec("Example", VersionSpec(v"55"))])
+    catch e
+        @test contains(sprint(showerror, e), "Example")
+    end
+
     nonexisting_pkg = randstring(14)
     @test_throws CommandError Pkg3.API.add(nonexisting_pkg)
     @test_throws CommandError Pkg3.API.up(nonexisting_pkg)
